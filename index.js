c464cc3922578f92349bae496dad08f2c477700d
João Eduardo <joaojaiba2023@gmail.com>
2025-02-18 12:54:45 -0300

    adicionando um get na api

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Olá, Arthur e colegas do BSI!");
+    res.send("Olá Arthur e colegas do BSI!");
+});
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Bem-vindo à minha API!' });
 });
 
 app.listen(PORT, () => {
